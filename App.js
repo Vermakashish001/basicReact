d3748ef108823471e2aa7ebcb7844e480c93691a
Vermakashish001 <118152624+Vermakashish001@users.noreply.github.com>
2024-01-30 22:10:53 +0530

    commit

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 const Parent = React.createElement("div",{id:"Parent"},
                 React.createElement("div",{id:"child"},
-                React.createElement("h1",{},"i am h1 tag")));
+                [React.createElement("h1",{},"i am h1 tag"),React.createElement("h2",{},"i am h2 tag")]));
 console.log(Parent);
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(Parent)
